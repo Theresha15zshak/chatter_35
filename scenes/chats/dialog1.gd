a1ff50e489a634275e5c78315ddc4f0dc832672d
Theresha15zshak <125734267+Theresha15zshak@users.noreply.github.com>
2024-07-10 16:22:23 +0100

    refoctor code

--- scenes/chats/dialog1.gd
+++ scenes/chats/dialog1.gd
@@ -1,14 +1,9 @@
 extends Control
-
+signal answers_ended
+signal animation_ended
 var dialog_json = {
 	"start": {
-		"text": "Привет! Пройди тест по майнкрафту",
-		"options": [
-			{"text": "next", "params": {"next": "start_1", "chance": 100}}
-		],
-	},
-	"start_1": {
-		"text": "Какое главное правило майнкрафта?",
+		"text": ["Привет! Пройди тест по майнкрафту", "Какое главное правило майнкрафта?"],
 		"options": [
 			{"text": "Не копать под себя", "params": {"next": "start_2", "chance": 100}},
 			{"text": "Не приближатся к криперу", "params": {"next": "end_bad", "chance": 100}},
@@ -16,7 +11,7 @@ var dialog_json = {
 		],
 	},
 	"start_2": {
-		"text": "Какой самый редкий ресурс в майнкрафте?",
+		"text": ["Какой самый редкий ресурс в майнкрафте?"],
 		"options": [
 			{"text": "Рудное золото", "params": {"next": "end_bad", "chance": 100}},
 			{"text": "Алмазы", "params": {"next": "end_bad", "chance": 100}},
@@ -25,14 +20,14 @@ var dialog_json = {
 		],
 	},
 	"start_3": {
-		"text": "Какая броня дает больше защиты кольчужная или золотая?",
+		"text": ["Какая броня дает больше защиты кольчужная или золотая?"],
 		"options": [
 			{"text": "Кольчужная", "params": {"next": "start_4", "chance": 100}},
 			{"text": "Золотая", "params": {"next": "end_bad", "chance": 100}}
 		],
 	},
 	"start_4": {
-		"text": "Какой моб майнкрафта самый маленький?",
+		"text": ["Какой моб майнкрафта самый маленький?"],
 		"options": [
 			{"text": "Зомби", "params": {"next": "end_bad", "chance": 100}},
 			{"text": "Камень", "params": {"next": "end_bad", "chance": 100}},
@@ -41,7 +36,7 @@ var dialog_json = {
 		],
 	},
 	"start_5": {
-		"text": "Какой моб майнкрафта может заражать других мобов?",
+		"text": ["Какой моб майнкрафта может заражать других мобов?"],
 		"options": [
 			{"text": "Зомби", "params": {"next": "start_6", "chance": 100}},
 			{"text": "Дракон", "params": {"next": "end_bad1", "chance": 100}},
@@ -49,7 +44,7 @@ var dialog_json = {
 		],
 	},
 	"start_6": {
-		"text": "Финальный вопрос. При прыжке на какой блок вы отпрыгните?",
+		"text": ["Финальный вопрос. При прыжке на какой блок вы отпрыгните?"],
 		"options": [
 			{"text": "Слизневый блок", "params": {"next": "end_good", "chance": 100}},
 			{"text": "Медовый блок", "params": {"next": "end_bad", "chance": 100}},
@@ -57,17 +52,17 @@ var dialog_json = {
 		],
 	},
 	"end_bad": {
-		"text": "К сожалению вы проиграли",
+		"text": ["К сожалению вы проиграли"],
 		"options": [],
 		"end_type": "lose"
 	},
 	"end_bad1": {
-		"text": "К сожалению вы проиграли",
+		"text": ["К сожалению вы проиграли"],
 		"options": [],
 		"end_type": "lose"
 	},
 	"end_good": {
-		"text": "Поздравляю! Вы прошли тест.",
+		"text": ["Поздравляю! Вы прошли тест."],
 		"options": [],
 		"end_type": "win"
 	}
@@ -84,7 +79,6 @@ onready var button1 = $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer
 onready var button2 = $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/control_button2/Button
 onready var button3 = $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer2/control_button/Button
 onready var button4 = $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer2/control_button2/Button
-
 
 
 const start_dialog_id = "start"
@@ -117,68 +111,78 @@ func get_lose_text():
 func _func_game_end_screen(result):
 	var game_over_screen_instance = game_over_screen.instance()
 	var game_win_screen_instance = game_win_screen.instance()
+	yield(get_tree().create_timer(0.861), "timeout")
 	if result == "lose":
 		add_child(game_over_screen_instance)
-	else:
+	elif result == "win":
 		add_child(game_win_screen_instance)
-
+	else:
+		push_error("Wrong end type {0}").format([result])
 	
 func hide_buttons():
-	var buttons = [button1, button2, button3 ,button4]
-	for i in range(4):
-		buttons[i].hide()
+	pass 
+	
 
+	for b in [button1, button2, button3 ,button4]:
+		b.hide()
+
+
+
+func play_animation(time_in_seconds):
+	var animation_instance = animation.instance()
+	get_node("MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer").add_child(animation_instance)
+	# Timer for animation
+	yield(get_tree().create_timer(time_in_seconds), "timeout")
+	# hiding animation
+	var animka = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/animation
+	animation_instance.queue_free()
+	yield(get_tree().create_timer(0.1), "timeout")
+	emit_signal("animation_ended")
+func draw_comp_answer(t):
+	var comp_dialog_text = text.instance()
+	get_node("MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer").add_child(comp_dialog_text)
+	comp_dialog_text.change_text(t)
+
+func draw_answer_options(options):
+	yield(self, "answers_ended")
+	var buttonstext = [button1text, button2text, button3text, button4text]
+	var buttons = [button1, button2, button3 ,button4]
+	for i in range(options.size()):
+		var option_text = options[i]["text"]
+		buttonstext[i].text = "{0}".format([option_text]) 
+		buttons[i].show()
+
+func draw_answers(dialog):
+	for t in dialog["text"]:
+		play_animation(0.5)
+		# Assigning text to text
+		yield(self, "animation_ended")
+		draw_comp_answer(t)
+	emit_signal("answers_ended")
+		
 func show_dialog(dialog_id):
+	if not (dialog_id in dialog_json):
+		push_error("Can't find dialog id")
+	
+	hide_buttons()	
 	current_dialog_id = dialog_id
-	if dialog_id in dialog_json:
-		hide_buttons()
-		# Declaring vars for dialog, instance of text and 
-		var dialog = dialog_json[dialog_id]
-		var dialog_text = text.instance()
-		#var nodes = get_node("VBoxContainer/ScrollContainer/VBoxContainer").get_child_count()
-		#Assigning text to button
-		options = dialog["options"]
-		if len(dialog["options"]) == 1:
-			if options[0]["text"] == "next":
-				get_node("MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer").add_child(dialog_text)
-				dialog_text.change_text(dialog["text"])
-				show_dialog(options[0]["params"]["next"])
-		else:
-			# Animation
-			var animation_instance = animation.instance()
-			get_node("MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer").add_child(animation_instance)
-			# Timer for animation
-			var time_in_seconds = 0.5
-			yield(get_tree().create_timer(time_in_seconds), "timeout")
-			# hiding animation
-			var animka = $MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer/animation
-			animka.queue_free()
-			# Assigning text to text
-			get_node("MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer").add_child(dialog_text)
-			dialog_text.change_text(dialog["text"])
+	# Declaring vars for dialog, instance of text and 
+	var dialog = dialog_json[dialog_id]
+
+	draw_answers(dialog)
+		
+	if len(dialog["options"]) == 0: 
+		_func_game_end_screen(dialog_json[dialog_id]["end_type"])
+	
+	options = dialog["options"]
+	draw_answer_options(options)
+	
 			
-			
-			# assigning text to button and show/hide buttons
-			
-			var buttonstext = [button1text, button2text, button3text, button4text]
-			var buttons = [button1, button2, button3 ,button4]
-			
-			for i in range(4):
-				if i < options.size():
-					var option_text = options[i]["text"]
-					print(typeof(option_text))
-					buttonstext[i].text = "{0}".format([option_text]) 
-					
-					buttons[i].show()
-				else:
-					buttons[i].hide()
-					
-					
-		if len(dialog["options"]) == 0: 
-			_func_game_end_screen(dialog_json[dialog_id]["end_type"])
-func next_action(chance, get_cahnce, dialog_to_show):
+func next_action(chance, get_cahnce, dialog_id_to_show):
 	if chance <= get_cahnce:
-		show_dialog(dialog_to_show)
+		show_dialog(dialog_id_to_show)
+	else:
+		_func_game_end_screen("lose")
 
 
 func _on_button_pressed():
@@ -188,7 +192,6 @@ func _on_button_pressed():
 	var text_answer_player = text_answer.instance() 
 	get_node("MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer").add_child(text_answer_player)
 	text_answer_player.change_text(options[index_i]["text"])
-	
 	
 	next_action(chance, options[index_i]["params"]["chance"], options[index_i]["params"]["next"])
 
@@ -228,3 +231,5 @@ func _on_button_4_pressed():
 
 func _on_button_menu_pressed():
 	get_tree().change_scene("res://scenes/controlmenu.tscn")
+
+
